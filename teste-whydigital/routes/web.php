ff154aba1f621d69159783dabf2ece3ae6e746cc
Diego <s.diego04@gmail.com>
2021-07-23 21:01:40 -0300

    adicionando rota para listagem de tarefas

--- teste-whydigital/routes/web.php
+++ teste-whydigital/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\listasDeTarefasController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//rota para a pagina onde exibe todas as tarefas do usuario
+Route::get('/tarefas',[listasDeTarefasController::class,'index'])
+    ->name('tarefas');
