7e1e13fd22a69aa2ebc7ee68f2f921b85c962602
Diego <s.diego04@gmail.com>
2021-07-24 13:51:30 -0300

    criado migration create tarefa

---
+++ teste-whydigital/database/migrations/2021_07_24_164126_create_tarefa.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarefa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefa', function (Blueprint $table) {
+            $table->id('id_tarefa');
+            $table->string('nome');
+            $table->boolean('estatus');
+            $table->integer('lista_id')->unsigned();
+            $table->foreign('lista_id')->references('id_lista')
+                ->on('lista')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefa');
+    }
+}
