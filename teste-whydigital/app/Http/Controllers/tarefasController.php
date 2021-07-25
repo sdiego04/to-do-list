21d0e3778efea0d19d88e7256e4b42f9a5bdecec
Diego <s.diego04@gmail.com>
2021-07-24 21:27:50 -0300

    criado metod store

--- teste-whydigital/app/Http/Controllers/tarefasController.php
+++ teste-whydigital/app/Http/Controllers/tarefasController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\tarefaRequest;
+use App\Services\salvarNovaTarefa;
 use Illuminate\Http\Request;
 
 class tarefasController extends Controller
 {
-    //
+    //metodo que ira salvar no banco uma nova tarefa
+    public function store(tarefaRequest $request,salvarNovaTarefa $salvarNovaTarefa){
+        $salvarNovaTarefa->salvarTarefa($request->nome,$request->descricao);
+    }
+    //metodo para chamar o form de cadastro de tarefas
+    public function formTarefa(){
+        return view('/viewsTarefas/formTarefa');
+    }
 }
