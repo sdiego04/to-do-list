5adfeeaddd219889e8327b4addf92d931b4348cb
Diego <s.diego04@gmail.com>
2021-07-27 01:52:30 -0300

    refatorado codigo

--- teste-whydigital/app/Http/Controllers/tarefasController.php
+++ teste-whydigital/app/Http/Controllers/tarefasController.php
@@ -9,11 +9,13 @@ use Illuminate\Http\Request;
 class tarefasController extends Controller
 {
     //metodo que ira salvar no banco uma nova tarefa
-    public function store(tarefaRequest $request,salvarNovaTarefa $salvarNovaTarefa){
-        $salvarNovaTarefa->salvarTarefa($request->nome,$request->descricao);
+    public function store(tarefaRequest $request,salvarNovaTarefa $salvarNovaTarefa,$id){
+        $salvarNovaTarefa->salvarTarefa($request->nome,$request->descricao,$id);
+        return redirect('/listas');
     }
     //metodo para chamar o form de cadastro de tarefas
-    public function formTarefa(){
-        return view('/viewsTarefas/formTarefa');
+    public function formTarefa(Request $request){
+        $id=$request->id_lista;
+        return view('/viewsTarefas/formTarefa',compact('id'));
     }
 }
