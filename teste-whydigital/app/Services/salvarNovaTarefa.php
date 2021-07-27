331bd45158be9a6b34c2493df7e5d4bd76ac90a0
Diego <s.diego04@gmail.com>
2021-07-27 01:54:27 -0300

    refatorado o codigo

--- teste-whydigital/app/Services/salvarNovaTarefa.php
+++ teste-whydigital/app/Services/salvarNovaTarefa.php
@@ -4,15 +4,21 @@
 namespace App\Services;
 
 //classe para auxiliar na manutenção do condigo
+use App\Models\Listas;
 use App\Models\Tarefas;
 use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Integer;
+use PhpParser\Node\Scalar\String_;
 
 class salvarNovaTarefa
 {
     //metodo para inserir no banco
-    public function salvarTarefa(string $nome,string $descricao){
-        DB::transaction(function ()use($nome,$descricao){
-            Tarefas::create();
-        });
+    public function salvarTarefa(string $nome,string $descricao,int $id){
+       $tarefas=new Tarefas();
+       $tarefas->nome=$nome;
+       $tarefas->descricao=$descricao;
+       $tarefas->lista_id=$id;
+       $tarefas->estatus=true;
+       $tarefas->save();
     }
 }
