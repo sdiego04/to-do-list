35acd4e86bd176ad70efa07f7591b4695e5c62d6
Diego <s.diego04@gmail.com>
2021-07-27 23:33:28 -0300

    organizado botoes

--- teste-whydigital/resources/views/viewsListas/formAlterarLista.blade.php
+++ teste-whydigital/resources/views/viewsListas/formAlterarLista.blade.php
@@ -10,25 +10,32 @@
 </head>
 <body>
     <div class="container">
-        <h3>{{request('nome')}}</h3><hr>
+        <h3>{{request('nome')}}</h3>
+        <div align="right">
+            <button title="voltar" onclick="document.location='/listas'" class="btn btn-primary bi bi-house"></button>
+        </div><hr>
     </div>
     <div class="container">
-        <form method="get">
+        <form method="get" action="/alterarlista/{{request('id_lista')}}">
             <div class="row">
             <div class="col">
                 <label>Nome da lista de tarefa</label>
-                <input type="text" value="{{request('nome')}}" class="form-control">
+                <input type="text" value="{{request('nome')}}" class="form-control" name="nome">
+                <span class="text text-danger">@error('nome'){{$message}}@enderror</span>
             </div>
             <div class="col">
                     <label>alterar a data de encerramento</label>
-                    <input type="date" value="{{request('dataentrega')}}" class="form-control">
+                    <input type="date" value="{{request('dataentrega')}}" class="form-control" name="dataentrega">
+                    <span class="text text-danger">@error('dataentrega'){{$message}}@enderror</span>
             </div>
             </div><br>
             <div class="row">
                 <div class="col" align="right">
-                    <button title="Salvar Alterações" class="btn btn-warning bi bi-clipboard-data"></button>
-                    <button title="Marcar como Realizada" class="btn btn-primary bi bi-clipboard-check"></button>
-                    <button title="Excluir Lista" class="btn btn-danger bi bi-clipboard-x"></button>
+                    <button type="submit" title="Salvar Alterações" class="btn btn-warning bi bi-clipboard-data"></button>
+                    @csrf
+        </form>
+                    <button type="button" onclick="document.location='/listaconcluida/{{request('id_lista')}}'" title="Marcar como Realizada" class="btn btn-primary bi bi-clipboard-check" confirmed></button>
+                    <button type="button" onclick="document.location='/excluir/{{request(('id_lista'))}}'" title="Excluir Lista" class="btn btn-danger bi bi-clipboard-x"></button>
                 </div>
             </div>
             <div class="row">
@@ -37,17 +44,27 @@
                     <button title="Salvar" type="button" class="btn btn-success bi bi-clipboard-plus" onclick="document.location='/novatarefa/{{request('id_lista')}}'"></button>
                 </div>
             </div>
-        </form><br><br>
+        <br><br>
             <?php foreach ($tarefa as $tarefa){ ?>
             <div class="row">
                 <div class="col">
-                    <span>Nome:{{$tarefa->nome}}</span><br>
+                    <span>Nome:{{$tarefa->nome}}</span>
+                    <?php if($tarefa->estatus==true){?>
+                        <small class="text text-muted">Tarefa Realizada</small>
+                    <?php }?>
+                    <br>
                     <span>Descrição:{{$tarefa->descricao}}</span><br><br>
                 </div>
                 <div class="col" align="right">
-                    <button title="Alterar Atividade" class="btn btn-warning bi bi-clipboard-data"></button>
-                    <button title="Marcar como Realizada" class="btn btn-primary bi bi-clipboard-check"></button>
-                    <button title="Excluir Atividade" class="btn btn-danger bi bi-clipboard-x"></button>
+                    <?php if($tarefa->estatus==false){?>
+                        <button type="button" onclick="document.location='/alterartarefa/{{$tarefa->id_tarefa}}'" title="Alterar Atividade" class="btn btn-warning bi bi-clipboard-data"></button>
+                        <button type="button" onclick="document.location='/tarefaconcluida/{{$tarefa->id_tarefa}}'" title="Marcar como Realizada" class="btn btn-primary bi bi-clipboard-check"></button>
+                        <button type="button" onclick="document.location='/excluirtarefa/{{$tarefa->id_tarefa}}'" title="Excluir Atividade" class="btn btn-danger bi bi-clipboard-x"></button>
+                    <?php }else{?>
+                        <button class="btn btn-secondary bi bi-clipboard-data"></button>
+                        <button type="button" onclick="document.location='/tarefainacabada/{{$tarefa->id_tarefa}}'" title="Marcar como Inacabada" class="btn btn-dark bi bi-clipboard"></button>
+                        <button class="btn btn-secondary bi bi-clipboard-x"></button>
+                    <?php }?>
                 </div>
             </div><hr>
             <?php }?>
