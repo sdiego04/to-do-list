4d610681f4415ff9645c39c0019dacfe8761fe50
Diego <s.diego04@gmail.com>
2021-07-29 22:53:23 -0300

    criado classe para auxiliar na organização do codigo

--- teste-whydigital/app/Services/listaAtivaEInativas.php
+++ teste-whydigital/app/Services/listaAtivaEInativas.php
@@ -4,9 +4,23 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Integer;
+
 class listaAtivaEInativas
 {
-    public function listaAtivaInativa(){
-        
+    public function listaAtiva(int $id){
+            $r= DB::table('tarefa')
+                ->where('estatus','=',true)
+                ->where('lista_id','=',$id)
+                ->count('estatus');
+            return $r;
+    }
+    public function listaInativa(int $id){
+        $r= DB::table('tarefa')
+            ->where('estatus','=',false)
+            ->where('lista_id','=',$id)
+            ->count('estatus');
+        return $r;
     }
 }
